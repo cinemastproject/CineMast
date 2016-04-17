7c51189266721d2e4294026b1409d8f0e497f45a
Sumit-Kumar14 <sumitgupta.gupta14@gmail.com>
2016-04-18 01:18:51 +0530

    Popular Movies

--- app/src/main/java/com/cinemast/cinemast/HomePage.java
+++ app/src/main/java/com/cinemast/cinemast/HomePage.java
@@ -1,44 +1,20 @@
 package com.cinemast.cinemast;
 
-import android.app.ProgressDialog;
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.GridView;
-import android.widget.Toast;
 
-import java.util.List;
-
-public class HomePage extends AppCompatActivity implements FetchFromServerUser{
-
-    ProgressDialog progressDialog;
+public class HomePage extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        new FetchFromServerTask(this, 0).execute("http://api.themoviedb.org/3/movie/upcoming?api_key=YOUR_KEY");
-    }
+        Fragment popularMoviesFragment = new PopularMoviesFragment();
+        getFragmentManager().beginTransaction().replace(R.id.popularMoviesFragment, popularMoviesFragment).commit();
 
-    @Override
-    public void onPreFetch() {
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Fetching Details");
-        progressDialog.setIndeterminate(false);
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        progressDialog.setCancelable(false);
-        progressDialog.show();
-    }
-
-    @Override
-    public void onFetchCompletion(String string, int id) {
-        if(progressDialog != null)
-            progressDialog.dismiss();
-        PopularMovieParser parser = new PopularMovieParser(string);
-        List<PopularMovieBean> popularMoviesList = parser.getMoviesList();
-        GridView moviesGrid = (GridView)findViewById(R.id.movies);
-        MoviesAdapter adapter = new MoviesAdapter(this, popularMoviesList);
-        moviesGrid.setAdapter(adapter);
+        Fragment upcomingMoviesFragment = new UpcomingFragment();
+        getFragmentManager().beginTransaction().replace(R.id.upcomingMoviesFragment, upcomingMoviesFragment).commit();
     }
 }
--- app/src/main/java/com/cinemast/cinemast/MoviesAdapter.java
+++ app/src/main/java/com/cinemast/cinemast/MoviesAdapter.java
@@ -1,6 +1,8 @@
 package com.cinemast.cinemast;
 
 import android.content.Context;
+import android.media.Image;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +14,23 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class MoviesAdapter extends BaseAdapter{
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder>{
 
     Context context;
     List<PopularMovieBean> list;
     LayoutInflater inflater;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView title;
+        private ImageView poster;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title);
+            poster = (ImageView) itemView.findViewById(R.id.moviePoster);
+        }
+    }
 
     public MoviesAdapter(Context context, List<PopularMovieBean> list){
         this.context = context;
@@ -25,29 +39,25 @@ public class MoviesAdapter extends BaseAdapter{
     }
 
     @Override
-    public int getCount() {
-        return list.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return list.get(position);
-    }
-
-    @Override
     public long getItemId(int position) {
         return 0;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public int getItemCount() {
+        return list.size();
+    }
 
-        convertView = inflater.inflate(R.layout.new_released_movies_item, parent, false);
-        ImageView poster = (ImageView) convertView.findViewById(R.id.moviePoster);
-        //TextView title = (TextView) convertView.findViewById(R.id.title);
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.new_released_movies_item, parent, false);
+        return new ViewHolder(view);
+    }
 
-        Picasso.with(context).load("http://image.tmdb.org/t/p/w342/" + list.get(position).getPoster_path()).into(poster);
-        //title.setText(list.get(position).getTitle());
-        return convertView;
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.title.setText(list.get(position).getTitle());
+        if(list.get(position).getPoster_path() != null)
+            Picasso.with(context).load("http://image.tmdb.org/t/p/w185/" + list.get(position).getPoster_path()).into(holder.poster);
     }
 }
