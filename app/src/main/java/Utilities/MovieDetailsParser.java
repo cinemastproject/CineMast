7bf53c8ab706d4d5dff28b1da20a5aa153500f5f
DJpatowary <deepjyoti90@gmail.com>
2016-05-09 23:42:36 +0530

    Detail

---
+++ app/src/main/java/Utilities/MovieDetailsParser.java
@@ -0,0 +1,73 @@
+package Utilities;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by deepjyoti on 9/5/16.
+ */
+public class MovieDetailsParser {
+    String result;
+
+    public MovieDetailsParser(String result){
+        this.result = result;
+    }
+
+    public MovieDetailsBean getMovieDetailsList(){
+        MovieDetailsBean movieDetailsBean = new MovieDetailsBean();
+        try {
+            JSONObject root = new JSONObject(result);
+            boolean adult = root.getBoolean("adult");
+            String backdrop_path = root.getString("backdrop_path");
+            JSONArray genresList = root.getJSONArray("genres");
+            String[] genreArray = new String[genresList.length()];
+            for (int i = 0; i < genresList.length(); i++){
+                JSONObject genre = genresList.getJSONObject(i);
+                genreArray[i] = genre.getString("name");
+            }
+            String homepage = root.getString("homepage");
+            String id = root.getString("id");
+            String imdb_id = root.getString("imdb_id");
+            String original_language = root.getString("original_language");
+            String original_title = root.getString("original_title");
+            String overview = root.getString("overview");
+            double popularity = root.getDouble("popularity");
+            String poster_path = root.getString("poster_path");
+            String release_date = root.getString("release_date");
+            int runtime = root.getInt("runtime");
+            String status = root.getString("status");
+            String tagline = root.getString("tagline");
+            String title = root.getString("title");
+            boolean video = root.getBoolean("video");
+            int vote_average = root.getInt("vote_average");
+            int vote_count = root.getInt("vote_count");
+
+            movieDetailsBean.setAdult(adult);
+            movieDetailsBean.setBackdrop_path(backdrop_path);
+            movieDetailsBean.setGenres(genreArray);
+            movieDetailsBean.setHomepage(homepage);
+            movieDetailsBean.setId(id);
+            movieDetailsBean.setImdb_id(imdb_id);
+            movieDetailsBean.setOriginal_language(original_language);
+            movieDetailsBean.setOriginal_title(original_title);
+            movieDetailsBean.setOverview(overview);
+            movieDetailsBean.setPopularity(popularity);
+            movieDetailsBean.setPoster_path(poster_path);
+            movieDetailsBean.setRelease_date(release_date);
+            movieDetailsBean.setRuntime(runtime);
+            movieDetailsBean.setStatus(status);
+            movieDetailsBean.setTagline(tagline);
+            movieDetailsBean.setTitle(title);
+            movieDetailsBean.setVideo(video);
+            movieDetailsBean.setVote_average(vote_average);
+            movieDetailsBean.setVote_count(vote_count);
+
+        }catch (JSONException jex){
+
+        }
+        return movieDetailsBean;
+
+    }
+
+}
--- app/src/main/java/com/cinemast/cinemast/MovieDetails.java
+++ app/src/main/java/com/cinemast/cinemast/MovieDetails.java
@@ -3,11 +3,32 @@ package com.cinemast.cinemast;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MovieDetails extends AppCompatActivity {
+import Utilities.FetchFromServerTask;
+import Utilities.FetchFromServerUser;
+import Utilities.MovieDetailsParser;
+
+public class MovieDetails extends AppCompatActivity implements FetchFromServerUser {
+    View view;
+    TextView genre;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_detail);
+        genre = (TextView) findViewById(R.id.genre)
+        new FetchFromServerTask(this, 0).execute("https://api.themoviedb.org/3/movie/now_playing?api_key=0d9b1f55e11c548f66e11f78a7f38357");
+
+    }
+
+    @Override
+    public void onPreFetch() {
+
+    }
+
+    @Override
+    public void onFetchCompletion(String string, int id) {
+        MovieDetailsParser = 
     }
 }
