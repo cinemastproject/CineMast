668de01b754589b5fe89e3ef2f6c19e0d1f0218c
Sumit-Kumar14 <sumitgupta.gupta14@gmail.com>
2016-05-13 16:30:14 +0530

    People

---
+++ app/src/main/java/Utilities/PersonImagesBean.java
@@ -0,0 +1,63 @@
+package Utilities;
+
+import java.io.Serializable;
+
+/**
+ * Created by suny on 13/5/16.
+ */
+public class PersonImagesBean implements Serializable {
+    private float aspect_ratio;
+    private String file_path;
+    private int height;
+    private float vote_average;
+    private int vote_count;
+    private int width;
+
+    public float getAspect_ratio() {
+        return aspect_ratio;
+    }
+
+    public void setAspect_ratio(float aspect_ratio) {
+        this.aspect_ratio = aspect_ratio;
+    }
+
+    public String getFile_path() {
+        return file_path;
+    }
+
+    public void setFile_path(String file_path) {
+        this.file_path = file_path;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public float getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(float vote_average) {
+        this.vote_average = vote_average;
+    }
+
+    public int getVote_count() {
+        return vote_count;
+    }
+
+    public void setVote_count(int vote_count) {
+        this.vote_count = vote_count;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
